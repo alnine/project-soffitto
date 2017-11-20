1de4ac9ba8b7a4a23961f22616117f9de19ec540
Oleksandr <aleksandr.nine@gmail.com>
2017-11-21 00:00:46 +0200

    Created marker

--- js/init-map.js
+++ js/init-map.js
@@ -1,11 +1,17 @@
 function initMap() {
         var uluru = {lat: 48.516513, lng: 34.612297};
+        var markerImage =  {
+          url: 'img/map-marker.svg',
+          size: new google.maps.Size(44, 58),
+        };
+
         var map = new google.maps.Map(document.getElementById('feedback-map-wrapper'), {
           zoom: 18,
           center: uluru
         });
         var marker = new google.maps.Marker({
           position: uluru,
-          map: map
+          map: map,
+          icon: markerImage
         });
       }
